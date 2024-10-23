13bf7a8ea1245ae0fffdf52d3d724d45935f09bf
AissaRouk <aissa.rouk@gmail.com>
2024-10-23 21:52:27 +0300

    Recipe type added

--- src/Types/Types.tsx
+++ src/Types/Types.tsx
@@ -3,3 +3,12 @@ export type Ingredient = {
   name: string;
   category: string;
 };
+
+export type Recipe = {
+  id: number;
+  name: string;
+  link: string;
+  ingredients: Ingredient[];
+  preparationTime: number;
+  servingSize: number;
+};
