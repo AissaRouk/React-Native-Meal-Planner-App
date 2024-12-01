89ff03eb18f9228a253f160698b31c95992f979c
AissaRouk <aissa.rouk@gmail.com>
2024-12-01 15:43:47 +0200

    added color change when selected feature

--- src/Components/HeaderComponent.tsx
+++ src/Components/HeaderComponent.tsx
@@ -1,10 +1,21 @@
-// Header.tsx
-import React from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 
 const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-export const Header = () => {
+const Header = () => {
+  const [selectedDay, setSelectedDay] = useState<string | null>(null);
+
+  const handleDayPress = (day: string) => {
+    setSelectedDay(day);
+  };
+
   return (
     <View style={styles.headerContainer}>
       <View style={styles.titleContainer}>
@@ -15,9 +26,21 @@ export const Header = () => {
         style={styles.scrollView}
         showsHorizontalScrollIndicator={false}>
         {daysOfWeek.map(day => (
-          <View key={day} style={styles.dayCard}>
-            <Text style={styles.dayText}>{day}</Text>
-          </View>
+          <TouchableOpacity
+            key={day}
+            onPress={() => handleDayPress(day)}
+            style={[
+              styles.dayCard,
+              selectedDay === day && styles.selectedDayCard,
+            ]}>
+            <Text
+              style={[
+                styles.dayText,
+                selectedDay === day && styles.selectedDayText,
+              ]}>
+              {day}
+            </Text>
+          </TouchableOpacity>
         ))}
       </ScrollView>
     </View>
@@ -49,12 +72,18 @@ const styles = StyleSheet.create({
     borderColor: '#e6e6eb',
     borderWidth: 0.5,
     borderRadius: 5,
-    marginHorizontal: 8,
+    marginRight: 16,
+  },
+  selectedDayCard: {
+    backgroundColor: '#fb7945',
   },
   dayText: {
     color: 'black',
     fontSize: 18,
     fontWeight: 'bold',
+  },
+  selectedDayText: {
+    color: 'white', // Make the text more readable with the new background
   },
 });
 
