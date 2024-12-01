946b7e41046e4880743036892a2165e2c088b1fa
AissaRouk <aissa.rouk@gmail.com>
2024-12-01 14:41:08 +0200

    react-native-navigation added with the NavigationContainer and RootStack

--- App.tsx
+++ App.tsx
@@ -1,32 +1,26 @@
-import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
-import {createTables, createIngredient} from './src/services/db-services';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import MainScreen from './src/Screens/MainScreen';
+
+export const MainScreenName = 'MainScreen';
+
+const Stack = createNativeStackNavigator();
+
+const RootStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{headerShown: false}}
+      initialRouteName={MainScreenName}>
+      <Stack.Screen name={MainScreenName} component={MainScreen} />
+    </Stack.Navigator>
+  );
+};
 
 function App(): React.JSX.Element {
-  useEffect(() => {
-    // Initialize tables and insert ingredient
-    const initializeDatabase = async () => {
-      try {
-        // Create tables
-        await createTables();
-        console.log('Tables created successfully.');
-
-        // Insert a test ingredient (for example, 'Tomato' under category 'Vegetable')
-        await createIngredient('Tomato', 'Vegetable');
-        console.log('Ingredient inserted successfully.');
-      } catch (error) {
-        console.error('Error during initialization:', error);
-      }
-    };
-
-    // Call the function to initialize the database and add data
-    initializeDatabase();
-  }, []);
-
   return (
-    <View>
-      <Text>Database Initialized. Check the logs for results.</Text>
-    </View>
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
   );
 }
 
