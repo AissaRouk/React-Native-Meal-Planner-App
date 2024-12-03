65b2627e1a0d50af1df5b143b87fda8d766142ad
AissaRouk <aissa.rouk@gmail.com>
2024-12-03 22:01:47 +0200

    redesigned recipe card to look as it should

--- src/Components/RecipeCardComponent.tsx
+++ src/Components/RecipeCardComponent.tsx
@@ -1,18 +1,18 @@
 // RecipeCard.tsx
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import {Recipe} from '../Types/Types';
 
 const RecipeCard = ({recipe}: {recipe: Recipe}) => (
   <View style={styles.card}>
-    <Text style={styles.cardTitle}>Recipe</Text>
-    <Text style={styles.text}>ID: {recipe.id}</Text>
-    <Text style={styles.text}>Name: {recipe.name}</Text>
-    <Text style={styles.text}>Link: {recipe.link}</Text>
-    <Text style={styles.text}>
-      Preparation Time: {recipe.preparationTime} mins
-    </Text>
-    <Text style={styles.text}>Serving Size: {recipe.servingSize}</Text>
+    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+      <Image
+        source={require('../assets/icons/breakfast_icon.png')}
+        style={{height: 100}}
+        resizeMode="contain"
+      />
+      <Text style={styles.cardTitle}>{recipe.name}</Text>
+    </View>
   </View>
 );
 
@@ -20,17 +20,18 @@ const styles = StyleSheet.create({
   card: {
     backgroundColor: '#ffffff',
     padding: 16,
-    borderRadius: 8,
+    borderRadius: 20,
     marginBottom: 16,
     elevation: 2,
     borderColor: '#ddd',
     borderWidth: 1,
   },
   cardTitle: {
-    fontSize: 18,
+    fontSize: 25,
+    flex: 1,
     fontWeight: 'bold',
-    marginBottom: 8,
-    color: '#555',
+    color: 'black',
+    marginLeft: 10,
   },
   text: {
     fontSize: 16,
