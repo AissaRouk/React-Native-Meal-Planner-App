2ce7b491be301ce170d8481a7fb827d7bb768b2a
AissaRouk <aissa.rouk@gmail.com>
2024-12-05 21:17:16 +0200

    added the CRUD operations for the weeklyMeal Table

--- src/services/db-services.tsx
+++ src/services/db-services.tsx
@@ -29,6 +29,7 @@ export const TABLE_GROCERY_LIST = 'GroceryList';
 export const TABLE_PANTRY = 'Pantry';
 export const TABLE_INGREDIENT_PANTRY = 'IngredientPantry';
 export const TABLE_CATEGORY = 'Category';
+export const TABLE_WEEKLY_MEALS = 'WeeklyMeals';
 
 // Fields for the Recipe table
 export const RECIPE_ID = 'id';
@@ -62,6 +63,12 @@ export const INGREDIENT_PANTRY_ID = 'id';
 export const INGREDIENT_PANTRY_INGREDIENT_ID = 'ingredientId';
 export const INGREDIENT_PANTRY_QUANTITY = 'quantity';
 export const INGREDIENT_PANTRY_QUANTITY_TYPE = 'quantityType';
+
+// Fields for the WeeklyMeals table
+export const WEEKLY_MEALS_ID = 'id';
+export const WEEKLY_MEALS_DAY = 'day';
+export const WEEKLY_MEALS_MEAL_TYPE = 'mealType';
+export const WEEKLY_MEALS_RECIPE_ID = 'recipeId';
 
 //Ingredient CRUD functions
 
@@ -1096,6 +1103,251 @@ export const deleteIngredientPantry: (
       'deleteIngredientPantry -> error while deleting IngredientPantry: ' +
         JSON.stringify(error),
     );
+  }
+};
+
+/**
+ *
+ *
+ *
+ *
+ * WEEKLYMEALS TABLE CRUD FUNCTIONS
+ */
+
+/**
+ * Creates the WeeklyMeals table in the database if it does not already exist.
+ *
+ * The WeeklyMeals table contains information about which recipe is assigned to each day and meal type (e.g., Breakfast, Lunch, Dinner).
+ *
+ * @async
+ * @function createWeeklyMealsTable
+ * @returns {Promise<void>} Resolves when the table is created successfully or if it already exists.
+ */
+
+export const createWeeklyMealsTable: () => Promise<void> = async () => {
+  try {
+    const db = await getDbConnection();
+
+    const sqlCreateTable = `
+      CREATE TABLE IF NOT EXISTS ${TABLE_WEEKLY_MEALS} (
+        ${WEEKLY_MEALS_ID} INTEGER PRIMARY KEY AUTOINCREMENT,
+        ${WEEKLY_MEALS_DAY} TEXT NOT NULL,
+        ${WEEKLY_MEALS_MEAL_TYPE} TEXT NOT NULL,
+        ${WEEKLY_MEALS_RECIPE_ID} INTEGER,
+        FOREIGN KEY (${WEEKLY_MEALS_RECIPE_ID}) REFERENCES ${TABLE_RECIPE}(${RECIPE_ID})
+      );
+    `;
+
+    await db.transaction(tx => {
+      tx.executeSql(
+        sqlCreateTable,
+        [],
+        () => {
+          console.log(
+            'createWeeklyMealsTable -> Table created successfully or already exists.',
+          );
+        },
+        (_, error) => {
+          console.error(
+            'createWeeklyMealsTable -> SQL error in table creation:',
+            error,
+          );
+        },
+      );
+    });
+  } catch (error) {
+    console.error('createWeeklyMealsTable -> Transaction failed:', error);
+    throw new Error(`createWeeklyMealsTable: ${error}`);
+  }
+};
+
+/**
+ * Adds a new entry to the WeeklyMeals table.
+ *
+ * This function inserts a new row into the WeeklyMeals table, assigning a recipe to a specific day and meal type.
+ * If the same entry already exists, the function ignores the insertion.
+ *
+ * @async
+ * @function addWeeklyMeal
+ * @param {string} day - The day of the week (e.g., "Monday").
+ * @param {string} mealType - The type of meal (e.g., "Breakfast", "Lunch", "Dinner").
+ * @param {number} recipeId - The ID of the recipe to be assigned.
+ * @returns {Promise<void>} Resolves when the weekly meal is added successfully or the insertion is ignored.
+ */
+export const addWeeklyMeal: (
+  day: string,
+  mealType: string,
+  recipeId: number,
+) => Promise<void> = async (day, mealType, recipeId) => {
+  try {
+    const db = await getDbConnection();
+
+    const sqlInsert = `
+      INSERT OR IGNORE INTO ${TABLE_WEEKLY_MEALS} (
+        ${WEEKLY_MEALS_DAY},
+        ${WEEKLY_MEALS_MEAL_TYPE},
+        ${WEEKLY_MEALS_RECIPE_ID}
+      ) VALUES (?, ?, ?);
+    `;
+
+    await db.transaction(tx => {
+      tx.executeSql(
+        sqlInsert,
+        [day, mealType, recipeId],
+        (tx, results) => {
+          if (results.rowsAffected > 0) {
+            console.log('addWeeklyMeal -> Meal added successfully!');
+          } else {
+            console.log('addWeeklyMeal -> Meal not added or already exists.');
+          }
+        },
+        error => {
+          console.error(
+            'addWeeklyMeal -> SQL error in adding weekly meal:',
+            error,
+          );
+        },
+      );
+    });
+  } catch (error) {
+    console.error('addWeeklyMeal -> Transaction failed:', error);
+    throw new Error(`addWeeklyMeal: ${error}`);
+  }
+};
+
+/**
+ * Fetches all entries from the WeeklyMeals table.
+ *
+ * This function retrieves all weekly meal assignments from the WeeklyMeals table.
+ *
+ * @async
+ * @function getWeeklyMeals
+ * @returns {Promise<any[]>} Resolves with an array of weekly meal entries.
+ */
+
+export const getWeeklyMeals: () => Promise<any[]> = async () => {
+  try {
+    const db = await getDbConnection();
+    const sqlSelect = `SELECT * FROM ${TABLE_WEEKLY_MEALS}`;
+    const weeklyMeals: any[] = [];
+
+    await db.transaction(tx => {
+      tx.executeSql(
+        sqlSelect,
+        [],
+        (tx, resultSet) => {
+          const len = resultSet.rows.length;
+          for (let i = 0; i < len; i++) {
+            const row = resultSet.rows.item(i);
+            weeklyMeals.push(row);
+          }
+        },
+        error => {
+          console.error(
+            'getWeeklyMeals -> SQL error fetching weekly meals:',
+            error,
+          );
+        },
+      );
+    });
+
+    return weeklyMeals;
+  } catch (error) {
+    console.error('getWeeklyMeals -> Transaction error:', error);
+    return [];
+  }
+};
+
+/**
+ * Updates an existing entry in the WeeklyMeals table.
+ *
+ * This function updates a row in the WeeklyMeals table with new values for the day, meal type, and recipe ID.
+ *
+ * @async
+ * @function updateWeeklyMeal
+ * @param {number} id - The ID of the weekly meal entry to be updated.
+ * @param {string} day - The updated day of the week (e.g., "Monday").
+ * @param {string} mealType - The updated type of meal (e.g., "Breakfast", "Lunch", "Dinner").
+ * @param {number} recipeId - The updated recipe ID.
+ * @returns {Promise<void>} Resolves when the weekly meal entry is updated successfully.
+ */
+export const updateWeeklyMeal: (
+  id: number,
+  day: string,
+  mealType: string,
+  recipeId: number,
+) => Promise<void> = async (id, day, mealType, recipeId) => {
+  try {
+    const db = await getDbConnection();
+
+    const sqlUpdate = `
+      UPDATE ${TABLE_WEEKLY_MEALS}
+      SET ${WEEKLY_MEALS_DAY} = ?, ${WEEKLY_MEALS_MEAL_TYPE} = ?, ${WEEKLY_MEALS_RECIPE_ID} = ?
+      WHERE ${WEEKLY_MEALS_ID} = ?;
+    `;
+
+    await db.transaction(tx => {
+      tx.executeSql(
+        sqlUpdate,
+        [day, mealType, recipeId, id],
+        (tx, results) => {
+          if (results.rowsAffected > 0) {
+            console.log('updateWeeklyMeal -> Meal updated successfully!');
+          } else {
+            console.log('updateWeeklyMeal -> Meal update failed.');
+          }
+        },
+        error => {
+          console.error(
+            'updateWeeklyMeal -> SQL error updating weekly meal:',
+            error,
+          );
+        },
+      );
+    });
+  } catch (error) {
+    console.error('updateWeeklyMeal -> Transaction failed:', error);
+    throw new Error(`updateWeeklyMeal: ${error}`);
+  }
+};
+
+/**
+ * Deletes an entry from the WeeklyMeals table by its ID.
+ *
+ * This function removes a row from the WeeklyMeals table based on the provided ID.
+ *
+ * @async
+ * @function deleteWeeklyMeal
+ * @param {number} id - The ID of the weekly meal entry to be deleted.
+ * @returns {Promise<void>} Resolves when the weekly meal entry is deleted successfully.
+ */
+export const deleteWeeklyMeal: (id: number) => Promise<void> = async id => {
+  try {
+    const db = await getDbConnection();
+    const sqlDelete = `DELETE FROM ${TABLE_WEEKLY_MEALS} WHERE ${WEEKLY_MEALS_ID} = ?;`;
+
+    await db.transaction(tx => {
+      tx.executeSql(
+        sqlDelete,
+        [id],
+        (tx, results) => {
+          if (results.rowsAffected > 0) {
+            console.log('deleteWeeklyMeal -> Meal deleted successfully!');
+          } else {
+            console.log('deleteWeeklyMeal -> Meal not found or delete failed.');
+          }
+        },
+        error => {
+          console.error(
+            'deleteWeeklyMeal -> SQL error deleting weekly meal:',
+            error,
+          );
+        },
+      );
+    });
+  } catch (error) {
+    console.error('deleteWeeklyMeal -> Transaction failed:', error);
+    throw new Error(`deleteWeeklyMeal: ${error}`);
   }
 };
 
