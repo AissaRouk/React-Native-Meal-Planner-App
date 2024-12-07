14aef36e6dd7f8873a463aa2ceba57481329aa43
AissaRouk <aissa.rouk@gmail.com>
2024-12-07 12:08:17 +0200

    DaysOfWeek Type added

--- src/Types/Types.tsx
+++ src/Types/Types.tsx
@@ -64,3 +64,13 @@ export enum MealType {
   LUNCH = 'Lunch',
   DINNER = 'Dinner',
 }
+
+export enum DaysOfWeek {
+  MONDAY = 'Monday',
+  TUESDAY = 'Tuesday',
+  WEDNESDAY = 'Wednesday',
+  THURSDAY = 'Thursday',
+  FRIDAY = 'Friday',
+  SATURDAY = 'Saturday',
+  SUNDAY = 'Sunday',
+}
