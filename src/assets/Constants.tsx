0b254a3cc82ffff360ac6ccfeae358e65ab64846
AissaRouk <aissa.rouk@gmail.com>
2024-12-07 12:17:07 +0200

    added Constant file with daysOfWeekArray and mealTypeArray constants

---
+++ src/assets/Constants.tsx
@@ -0,0 +1,17 @@
+import {DaysOfWeek, MealType} from '../Types/Types';
+
+export const daysOfWeekArray: DaysOfWeek[] = [
+  DaysOfWeek.MONDAY,
+  DaysOfWeek.TUESDAY,
+  DaysOfWeek.WEDNESDAY,
+  DaysOfWeek.THURSDAY,
+  DaysOfWeek.FRIDAY,
+  DaysOfWeek.SATURDAY,
+  DaysOfWeek.SUNDAY,
+];
+
+export const mealTypeArray: MealType[] = [
+  MealType.BREAKFAST,
+  MealType.LUNCH,
+  MealType.DINNER,
+];
