55e738830f20c2bec5f74358a062eb32e7706412
AissaRouk <aissa.rouk@gmail.com>
2025-05-26 09:24:47 +0300

    added RecipeScreen, still needs improvement

---
+++ src/Screens/RecipesScreen.tsx
@@ -0,0 +1,28 @@
+import {ScrollView, Text, View} from 'react-native';
+import AppHeader from '../Components/AppHeader';
+import {useContext} from 'react';
+import {useAppContext} from '../Context/Context';
+import RecipeCard from '../Components/RecipeCardComponent';
+import {screensBackgroundColor} from '../Utils/Styiling';
+
+export function RecipesScreen(): React.JSX.Element {
+  const {recipes, setRecipes} = useAppContext();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: screensBackgroundColor,
+        padding: 16,
+      }}>
+      <View style={{marginBottom: 16}}>
+        <AppHeader title="Recipes" />
+      </View>
+      <ScrollView>
+        {recipes.map((recipe, index) => (
+          <RecipeCard key={index} recipe={recipe} />
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
