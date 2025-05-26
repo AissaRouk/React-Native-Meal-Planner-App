e756e12a72d774c21def7ee234a166cf62dadeba
AissaRouk <aissa.rouk@gmail.com>
2025-05-26 12:59:43 +0300

    added the RecipeScreenName screen

--- App.tsx
+++ App.tsx
@@ -3,9 +3,11 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainScreen from './src/Screens/MainScreen';
 import {AppProvider} from './src/Context/Context';
 import {RecipesScreen} from './src/Screens/RecipesScreen';
+import {RecipeScreen} from './src/Screens/RecipeScreen';
 
 export const MainScreenName: string = 'MainScreen';
 export const RecipesScreenName: string = 'RecipesScreen';
+export const RecipeScreenName: string = 'RecipeScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,6 +18,7 @@ const RootStack = () => {
       initialRouteName={MainScreenName}>
       <Stack.Screen name={MainScreenName} component={MainScreen} />
       <Stack.Screen name={RecipesScreenName} component={RecipesScreen} />
+      <Stack.Screen name={RecipeScreenName} component={RecipeScreen} />
     </Stack.Navigator>
   );
 };
