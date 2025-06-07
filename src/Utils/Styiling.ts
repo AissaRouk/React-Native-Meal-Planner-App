14aa5cede5cd30d5462761771b5d10c26580a17a
AissaRouk <aissa.rouk@gmail.com>
2025-06-07 23:55:21 +0300

    added some new styles and changed how they're exported

--- src/Utils/Styiling.ts
+++ src/Utils/Styiling.ts
@@ -1,13 +1,28 @@
 import {ColorValue, StyleSheet} from 'react-native';
 
-export const orangeBackgroundColor: ColorValue = '#fb7945';
-export const greyBorderColor: ColorValue = '#ccc';
-export const screensBackgroundColor: ColorValue = '#fff';
+// Genric colors
+const orangeBackgroundColor: ColorValue = '#fb7945';
+const greyBorderColor: ColorValue = '#ccc';
+const screensBackgroundColor: ColorValue = '#fff';
+const modalSemiTransparentBg: ColorValue = 'rgba(0,0,0,0.5)';
+const modalWhiteBg: ColorValue = 'white';
+//Generic styles
+const modalBorderRadius: number = 10;
 
-export const genericStyles = StyleSheet.create({
+const genericStyles = StyleSheet.create({
   nameBold: {
     fontSize: 16, // Font size for the ingredient name
     color: 'black', // Black text color
     fontWeight: '500', // Medium font weight
   },
 });
+
+export {
+  orangeBackgroundColor,
+  genericStyles,
+  greyBorderColor,
+  modalSemiTransparentBg,
+  screensBackgroundColor,
+  modalWhiteBg,
+  modalBorderRadius,
+};
