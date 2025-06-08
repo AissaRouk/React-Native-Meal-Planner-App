cfd429711cbc336a3676b35e9aee20168b0ef281
AissaRouk <aissa.rouk@gmail.com>
2025-06-08 18:56:40 +0300

    added the RecipeScreen as a Stack Screen

--- App.tsx
+++ App.tsx
@@ -4,10 +4,12 @@ import MainScreen from './src/Screens/MainScreen';
 import {AppProvider} from './src/Context/Context';
 import {RecipesScreen} from './src/Screens/RecipesScreen';
 import {RecipeScreen} from './src/Screens/RecipeScreen';
+import PantryScreen from './src/Screens/PantryScreen';
 
 export const MainScreenName: string = 'MainScreen';
 export const RecipesScreenName: string = 'RecipesScreen';
 export const RecipeScreenName: string = 'RecipeScreen';
+export const PantryScreenName: string = 'PanstryScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -19,6 +21,7 @@ const RootStack = () => {
       <Stack.Screen name={MainScreenName} component={MainScreen} />
       <Stack.Screen name={RecipesScreenName} component={RecipesScreen} />
       <Stack.Screen name={RecipeScreenName} component={RecipeScreen} />
+      <Stack.Screen name={PantryScreenName} component={PantryScreen} />
     </Stack.Navigator>
   );
 };
