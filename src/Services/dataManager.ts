2d939481a57a8561d4a98e76883034f5f58c4cc9
AissaRouk <aissa.rouk@gmail.com>
2025-06-09 15:01:15 +0300

    added the initialiseTables to initialise the tables if needed

--- src/Services/dataManager.ts
+++ src/Services/dataManager.ts
@@ -1,16 +1,33 @@
-import {fillTablesWMockupData, initialiseTables} from '../Screens/sucio';
 import {Ingredient} from '../Types/Types';
-import {getAllIngredients} from './ingredient-db-services';
+import {dropAllTables} from './db-services';
+import {
+  createIngredientTable,
+  getAllIngredients,
+} from './ingredient-db-services';
+import {createIngredientPantryTable} from './ingredientPantry-db-services';
+import {createPantryTable} from './pantry-db-services';
+import {createRecipeTable} from './recipe-db-services';
+import {createRecipeIngredientTable} from './recipeIngredients-db-services';
+import {createWeeklyMealsTable} from './weeklyMeals-db-services';
 
 //this function will initialize the whole database and fetch all the tables necessary to have everything ready for the program to work
 export const initialise: () => Promise<Ingredient[]> = async () => {
   try {
     await initialiseTables(); // Initializes the database tables
-    await fillTablesWMockupData(); // Fills the tables with mock data for testing
     const ingredients = await getAllIngredients(); // Fetches all the ingredients in the db
     console.log('fetched ingredients: ' + JSON.stringify(ingredients));
     return ingredients;
   } catch (error) {
     throw new Error('dataManager.initialize -> ' + error);
   }
+};
+
+export const initialiseTables = async () => {
+  // await dropAllTables();
+  await createIngredientTable();
+  await createRecipeTable();
+  await createRecipeIngredientTable();
+  await createWeeklyMealsTable();
+  await createPantryTable();
+  await createIngredientPantryTable();
 };
