7496ad3e77f40454c03a287546e40903d1d96215
AissaRouk <aissa.rouk@gmail.com>
2025-06-09 18:19:06 +0300

    added the Login and register screens with their firebase functionalities

--- App.tsx
+++ App.tsx
@@ -1,45 +1,59 @@
+// App.tsx
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainScreen from './src/Screens/MainScreen';
-import {AppProvider} from './src/Context/Context';
 import {RecipesScreen} from './src/Screens/RecipesScreen';
-import {RecipeScreen} from './src/Screens/RecipeScreen';
+import RecipeScreen from './src/Screens/RecipeScreen';
 import PantryScreen from './src/Screens/PantryScreen';
-import GroceryListScreen from './src/Components/GroceryListScreen';
-
-export const MainScreenName: string = 'MainScreen';
-export const RecipesScreenName: string = 'RecipesScreen';
-export const RecipeScreenName: string = 'RecipeScreen';
-export const PantryScreenName: string = 'PanstryScreen';
-export const GroceryListScreenName: string = 'GroceryListScreen';
+import GroceryListScreen from './src/Screens/GroceryListScreen';
+import LoginScreen from './src/Screens/LoginScreen';
+import RegisterScreen from './src/Screens/RegisterScreen';
+import {AppProvider} from './src/Context/Context';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
 
 const Stack = createNativeStackNavigator();
 
-const RootStack = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{headerShown: false}}
-      initialRouteName={MainScreenName}>
-      <Stack.Screen name={MainScreenName} component={MainScreen} />
-      <Stack.Screen name={RecipesScreenName} component={RecipesScreen} />
-      <Stack.Screen name={RecipeScreenName} component={RecipeScreen} />
-      <Stack.Screen name={PantryScreenName} component={PantryScreen} />
-      <Stack.Screen
-        name={GroceryListScreenName}
-        component={GroceryListScreen}
-      />
-    </Stack.Navigator>
-  );
-};
+export default function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
 
-function App(): React.JSX.Element {
+  useEffect(() => {
+    const unsubscribe = auth().onAuthStateChanged(u => {
+      setUser(u);
+      if (initializing) setInitializing(false);
+    });
+    return unsubscribe;
+  }, [initializing]);
+
+  // While waiting for Firebase to tell us if we're logged in or not…
+  if (initializing) return null;
+
   return (
     <AppProvider>
       <NavigationContainer>
-        <RootStack />
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          {user ? (
+            // logged in
+            <>
+              <Stack.Screen name="MainScreen" component={MainScreen} />
+              <Stack.Screen name="RecipesScreen" component={RecipesScreen} />
+              <Stack.Screen name="RecipeScreen" component={RecipeScreen} />
+              <Stack.Screen name="PantryScreen" component={PantryScreen} />
+              <Stack.Screen
+                name="GroceryListScreen"
+                component={GroceryListScreen}
+              />
+            </>
+          ) : (
+            // not logged in
+            <>
+              <Stack.Screen name="LoginScreen" component={LoginScreen} />
+              <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+            </>
+          )}
+        </Stack.Navigator>
       </NavigationContainer>
     </AppProvider>
   );
 }
-
-export default App;
