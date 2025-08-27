315766fb05c89eb2e2a7e4c599ba1d573566bef5
AissaRouk <aissa.rouk@gmail.com>
2025-08-27 20:13:23 +0300

    added duplicate name ingredient prevention

--- src/Services/ingredientPantry-db-services.ts
+++ src/Services/ingredientPantry-db-services.ts
@@ -1,88 +1,62 @@
-import {
-  getDbConnection,
-  TABLE_INGREDIENT,
-  TABLE_INGREDIENT_PANTRY,
-} from './db-services';
-import {INGREDIENT_ID} from './ingredient-db-services';
 import {IngredientPantry, IngredientPantryWithoutId} from '../Types/Types';
+import {
+  collection,
+  query,
+  getDocs,
+  getFirestore,
+  setDoc,
+  doc,
+  deleteDoc,
+  getDoc,
+  addDoc,
+  where,
+} from '@react-native-firebase/firestore';
 
 // Fields for the IngredientPantry table
 export const INGREDIENT_PANTRY_ID = 'id';
 export const INGREDIENT_PANTRY_INGREDIENT_ID = 'ingredientId';
 export const INGREDIENT_PANTRY_QUANTITY = 'quantity';
 export const INGREDIENT_PANTRY_QUANTITY_TYPE = 'quantityType';
+export const TABLE_INGREDIENT_PANTRY = 'IngredientPantry';
+
+const firestoreDb = getFirestore();
+const ingredientPantryCollection = collection(
+  firestoreDb,
+  TABLE_INGREDIENT_PANTRY,
+);
 
 /**
- *Function that creates the INGREDIENTPANTRY table
+ * Function that adds a pantry row to the Firestore table
  *
- * @returns {Promise<void>} A promise that resolves when the table is created successfully or if it already exists.
- */
-export const createIngredientPantryTable: () => Promise<void> = async () => {
-  try {
-    const db = await getDbConnection();
-
-    const sqlInsert = `CREATE TABLE IF NOT EXISTS ${TABLE_INGREDIENT_PANTRY} (
-    ${INGREDIENT_PANTRY_ID} INTEGER PRIMARY KEY AUTOINCREMENT,
-    ${INGREDIENT_PANTRY_INGREDIENT_ID} INTEGER UNIQUE,
-    ${INGREDIENT_PANTRY_QUANTITY} REAL,
-    ${INGREDIENT_PANTRY_QUANTITY_TYPE} STRING,
-    FOREIGN KEY (${INGREDIENT_PANTRY_INGREDIENT_ID}) REFERENCES ${TABLE_INGREDIENT}(${INGREDIENT_ID})
-    ) `;
-
-    await db.transaction(tx =>
-      tx.executeSql(sqlInsert, [], (tx, resultSet) => {
-        if (resultSet.rowsAffected > 0) {
-          console.log('createIngredientPantryTable -> Table created');
-        } else {
-          console.log('createIngredientPantryTable -> Table NOT created');
-        }
-      }),
-    );
-  } catch (error) {
-    throw new Error(
-      'Error in createIngredientPantryTable -> ' + JSON.stringify(error),
-    );
-  }
-};
-
-/**
- * Function that adds a pantry row to the table
- *
- * @param {IngredientWithoutId} ingredientPantry the ingredientPantry object but without id
- * @returns {Promise<void>} A promise that resolves when the table is created successfully or if it already exists.
+ * @param {IngredientPantryWithoutId} ingredientPantry the ingredientPantry object but without id
+ * @returns {Promise<void>} A promise that resolves when the ingredientPantry is added
  */
 export const addIngredientPantry: (
   ingredientPantry: IngredientPantryWithoutId,
 ) => Promise<void> = async ingredientPantry => {
   try {
-    const db = await getDbConnection();
-
-    // Corrected SQL insert statement
-    const sqlInsert = `INSERT OR IGNORE INTO ${TABLE_INGREDIENT_PANTRY} (
-      ${INGREDIENT_PANTRY_INGREDIENT_ID},
-      ${INGREDIENT_PANTRY_QUANTITY},
-      ${INGREDIENT_PANTRY_QUANTITY_TYPE}
-    ) VALUES (?, ?, ?);`;
-
-    await db.transaction(tx =>
-      tx.executeSql(
-        sqlInsert,
-        [
-          ingredientPantry.ingredientId,
-          ingredientPantry.quantity,
-          ingredientPantry.quantityType,
-        ],
-        (tx, resultSet) => {
-          if (resultSet.rowsAffected > 0) {
-            console.log('addIngredientPantry -> row added successfully');
-          } else {
-            console.log(
-              'addIngredientPantry -> there was a problem adding the row',
-            );
-          }
-        },
+    // Check if an ingredient with the same ingredientId already exists
+    const ingredientPantryQuery = query(
+      ingredientPantryCollection,
+      where(
+        INGREDIENT_PANTRY_INGREDIENT_ID,
+        '==',
+        ingredientPantry.ingredientId,
       ),
     );
+    const querySnapshot = await getDocs(ingredientPantryQuery);
+
+    if (!querySnapshot.empty) {
+      console.warn(
+        `Ingredient with ingredientId "${ingredientPantry.ingredientId}" already exists in pantry. Skipping.`,
+      );
+      return; // Exit the function if the ingredient already exists
+    }
+
+    // Generate a new document reference with an auto-generated ID
+    const docRef = doc(ingredientPantryCollection);
+    // Add the ingredientPantry with the generated ID as a prop
+    await setDoc(docRef, {...ingredientPantry, id: docRef.id});
   } catch (error) {
     throw new Error(
       'addIngredientPantry -> an error occurred while adding row: ' +
@@ -100,32 +74,16 @@ export const getAllIngredientPantriesDb: () => Promise<
   IngredientPantry[]
 > = async () => {
   try {
-    const db = await getDbConnection();
+    const firebaseIngredientPantries: IngredientPantry[] = [];
+    const ingredientPantriesQuery = query(ingredientPantryCollection);
+    const querySnapshot = await getDocs(ingredientPantriesQuery);
 
-    const sqlInsert = `SELECT * FROM ${TABLE_INGREDIENT_PANTRY}`;
+    querySnapshot.forEach((document: {data: () => IngredientPantry}) => {
+      const ingredientPantry = document.data() as IngredientPantry;
+      firebaseIngredientPantries.push(ingredientPantry);
+    });
 
-    const result: IngredientPantry[] = [];
-
-    await db.transaction(tx =>
-      tx.executeSql(sqlInsert, [], (tx, resultSet) => {
-        if (resultSet.rows.length > 0) {
-          var item: IngredientPantry;
-          for (var i = 0; i < resultSet.rows.length; i++) {
-            item = resultSet.rows.item(i);
-            result.push(item);
-          }
-        } else if (resultSet.rows.length == 0)
-          console.log(
-            'getIngredientPantries -> IngredientPantry Table is empty!',
-          );
-        else {
-          console.log(
-            "getIngredientPantries -> couldn't retrieve the IngredientPantries",
-          );
-        }
-      }),
-    );
-    return result;
+    return firebaseIngredientPantries;
   } catch (error) {
     throw new Error(
       "getIngredientPantries -> Couldn't retrieve the ingredientPantry " +
@@ -144,37 +102,12 @@ export const updateIngredientPantry: (
   updatedIngredientPantry: IngredientPantry,
 ) => Promise<void> = async updatedIngredientPantry => {
   try {
-    const db = await getDbConnection();
-
-    const sqlQuery = `UPDATE ${TABLE_INGREDIENT_PANTRY} SET ${INGREDIENT_PANTRY_INGREDIENT_ID} = ?, ${INGREDIENT_PANTRY_QUANTITY} = ?, ${INGREDIENT_PANTRY_QUANTITY_TYPE} = ? WHERE ${INGREDIENT_PANTRY_ID} = ?`;
-
-    console.log(
-      'updateIngredientPantry -> IngredientPantry update: ' +
-        JSON.stringify(updatedIngredientPantry),
+    const ingredientPantryDocRef = doc(
+      firestoreDb,
+      TABLE_INGREDIENT_PANTRY,
+      updatedIngredientPantry.id,
     );
-
-    await db.transaction(tx =>
-      tx.executeSql(
-        sqlQuery,
-        [
-          updatedIngredientPantry.ingredientId,
-          updatedIngredientPantry.quantity,
-          updatedIngredientPantry.quantityType,
-          updatedIngredientPantry.id,
-        ],
-        (tx, resultSet) => {
-          if (resultSet.rowsAffected > 0)
-            console.log(
-              'updateIngredientPantry -> IngredientPantry updated successfully :) -> ' +
-                JSON.stringify(resultSet.rows.item(0)),
-            );
-          else
-            console.log(
-              "updateIngredientPantry -> couldn't update IngredientPantry :(",
-            );
-        },
-      ),
-    );
+    await setDoc(ingredientPantryDocRef, updatedIngredientPantry);
   } catch (error) {
     throw new Error(
       'updateIngredientPantry -> error while updating the IngredientPantry: ' +
@@ -186,30 +119,19 @@ export const updateIngredientPantry: (
 /**
  * Function that deletes a specific ingredientPantry
  *
- * @param {number} ingredientPantryId - The id of the  ingredientPantry that will be deleted
+ * @param {string} ingredientPantryId - The id of the  ingredientPantry that will be deleted
  * @returns {Promise<void>} A promise that resolves when the table is created successfully or if it already exists.
  */
 export const deleteIngredientPantry: (
-  ingredientPantryId: number,
+  ingredientPantryId: string,
 ) => Promise<void> = async ingredientPantryId => {
   try {
-    const db = await getDbConnection();
-
-    const sqlQuery = `DELETE FROM ${TABLE_INGREDIENT_PANTRY} WHERE ${INGREDIENT_PANTRY_ID} = ?`;
-
-    await db.transaction(tx =>
-      tx.executeSql(sqlQuery, [ingredientPantryId], (tx, resultSet) => {
-        if (resultSet.rowsAffected > 0) {
-          console.log(
-            'deleteIngredientPantry -> IngredientPantry deleted successfully :)',
-          );
-        } else {
-          console.log(
-            "deleteIngredientPantry -> couldn't delete IngredientPantry :(",
-          );
-        }
-      }),
+    const ingredientPantryDoc = doc(
+      firestoreDb,
+      TABLE_INGREDIENT_PANTRY,
+      ingredientPantryId,
     );
+    await deleteDoc(ingredientPantryDoc);
   } catch (error) {
     throw new Error(
       'deleteIngredientPantry -> error while deleting IngredientPantry: ' +
