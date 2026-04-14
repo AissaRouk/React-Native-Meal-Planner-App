0ead13fcfd17dcb39fc72a64486bc5db4deed89d
Aissa Rouk <arouk@iguana-dms.com>
2026-04-14 11:46:12 +0300

    refactor(RecipesScreen): improve code formatting, readability and added empty reecipes message

--- src/Screens/RecipesScreen.tsx
+++ src/Screens/RecipesScreen.tsx
@@ -1,40 +1,30 @@
-import {ScrollView, View} from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import AppHeader from '../Components/AppHeader';
-import {useAppContext} from '../Context/Context';
+import { useAppContext } from '../Context/Context';
 import RecipeCard from '../Components/RecipeCardComponent';
-import {screensBackgroundColor} from '../Utils/Styiling';
-import {useNavigation} from '@react-navigation/native';
-import {RecipeScreenName} from '../../App';
-import {useState} from 'react';
-import {Recipe} from '../Types/Types';
-import {RecipeOptionsModal} from '../Components/RecipeOptionsModal';
+import { screensBackgroundColor } from '../Utils/Styiling';
+import { useNavigation } from '@react-navigation/native';
+import { RecipeScreenName } from '../../App';
+import React, { useState } from 'react';
+import { Recipe } from '../Types/Types';
+import { RecipeOptionsModal } from '../Components/RecipeOptionsModal';
 
 export function RecipesScreen(): React.JSX.Element {
-  const {recipes, deleteRecipe} = useAppContext();
+  const { recipes, deleteRecipe } = useAppContext();
   const navigation = useNavigation();
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedRecipe, setSelectedRecipe] = useState<Recipe>();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: screensBackgroundColor,
-        padding: 16,
-      }}>
-      <View style={{marginBottom: 16}}>
+    <View style={{ flex: 1, backgroundColor: screensBackgroundColor, padding: 16, }}>
+      <View style={{ marginBottom: 16 }}>
         <AppHeader title="Recipes" />
       </View>
-      <ScrollView>
+      <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
+        {recipes.length === 0 && <Text >No recipes yet!</Text>}
         {recipes.map((recipe, index) => (
-          <RecipeCard
-            key={index}
-            recipe={recipe}
-            onPress={() =>
-              (navigation as any).navigate(RecipeScreenName, {
-                recipe: recipe,
-              })
-            }
+          <RecipeCard key={index} recipe={recipe}
+            onPress={() => (navigation as any).navigate(RecipeScreenName, { recipe: recipe, })}
             onLongPress={() => {
               setModalVisible(true);
               setSelectedRecipe(recipe);
@@ -43,15 +33,8 @@ export function RecipesScreen(): React.JSX.Element {
         ))}
       </ScrollView>
       {selectedRecipe && (
-        <RecipeOptionsModal
-          recipe={selectedRecipe}
-          menuVisible={modalVisible}
-          deleteOption
-          onDelete={async () =>
-            await deleteRecipe(selectedRecipe.id).then(() => {
-              setModalVisible(false);
-            })
-          }
+        <RecipeOptionsModal recipe={selectedRecipe} menuVisible={modalVisible} deleteOption
+          onDelete={async () => await deleteRecipe(selectedRecipe.id).then(() => setModalVisible(false))}
           setMenuVisible={setModalVisible}
           key={selectedRecipe.id}
         />
